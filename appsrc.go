c39bbf7f31a9a4b6df7ebbd89f78e6a376673e3b
tshastry <tshastry@gmail.com>
2018-07-03 18:54:26 -0500

    Alternative appsrc bindings

--- appsrc.go
+++ appsrc.go
@@ -1,89 +1,42 @@
 package gst
 
-import (
-	"fmt"
-	"unsafe"
-)
-
 /*
-#cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0
-#cgo LDFLAGS: -lgstapp-1.0
 #include <stdlib.h>
-#include <string.h>
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
+
+#cgo LDFLAGS: -lgstapp-1.0
 */
 import "C"
 
-type FlowReturn C.GstFlowReturn
-
-const (
-	GST_FLOW_OK             = FlowReturn(C.GST_FLOW_OK)
-	GST_FLOW_FLUSHING       = FlowReturn(C.GST_FLOW_FLUSHING)
-	GST_FLOW_NOT_LINKED     = FlowReturn(C.GST_FLOW_NOT_LINKED)
-	GST_FLOW_NOT_NEGOTIATED = FlowReturn(C.GST_FLOW_NOT_NEGOTIATED)
-	GST_FLOW_ERROR          = FlowReturn(C.GST_FLOW_ERROR)
-	GST_FLOW_NOT_SUPPORTED  = FlowReturn(C.GST_FLOW_NOT_SUPPORTED)
+import (
+	"unsafe"
 )
 
-func (f FlowReturn) String() string {
-	switch f {
-	case GST_FLOW_OK:
-		return "GST_FLOW_OK"
-	case GST_FLOW_FLUSHING:
-		return "GST_FLOW_FLUSHING"
-	case GST_FLOW_NOT_LINKED:
-		return "GST_FLOW_NOT_LINKED"
-	case GST_FLOW_NOT_NEGOTIATED:
-		return "GST_FLOW_NOT_NEGOTIATED"
-	case GST_FLOW_ERROR:
-		return "GST_FLOW_ERROR"
-	case GST_FLOW_NOT_SUPPORTED:
-		return "GST_FLOW_NOT_SUPPORTED"
-	default:
-		return fmt.Sprintf("flow error: %d", f)
-	}
+type AppSrc struct {
+	BaseSrc
 }
 
-type AppSrc struct {
-	*Element
+func (b *AppSrc) g() *C.GstAppSrc {
+	return (*C.GstAppSrc)(b.GetPtr())
+}
+
+func (b *AppSrc) AsAppSrc() *AppSrc {
+	return b
 }
 
 func NewAppSrc(name string) *AppSrc {
-	element := ElementFactoryMake("appsrc", name)
-
-	element.SetProperty("is-live", true)
-	element.SetProperty("block", true)
-
-	return &AppSrc{element}
+	return (*AppSrc)(unsafe.Pointer(ElementFactoryMake("appsrc", name)))
 }
 
-func (a *AppSrc) g() *C.GstAppSrc {
-	return (*C.GstAppSrc)(a.GetPtr())
+func (b *AppSrc) PushBuffer(buffer *Buffer) int {
+	return (int)(C.gst_app_src_push_buffer((*C.GstAppSrc)(b.g()), (*C.GstBuffer)(buffer.GstBuffer)))
 }
 
-func (a *AppSrc) SetCaps(caps *Caps) {
-	p := unsafe.Pointer(caps) // HACK
-	C.gst_app_src_set_caps(a.g(), (*C.GstCaps)(p))
+func (b *AppSrc) EndOfStream() int {
+	return (int)(C.gst_app_src_end_of_stream((*C.GstAppSrc)(b.g())))
 }
 
-func (a *AppSrc) EOS() error {
-	ret := FlowReturn(C.gst_app_src_end_of_stream(a.g()))
-	if FlowReturn(ret) != GST_FLOW_OK {
-		return fmt.Errorf("appsrc eos: %v", ret)
-	}
-
-	return nil
-}
-
-func (a *AppSrc) Write(d []byte) (int, error) {
-	buf := C.gst_buffer_new_allocate(nil, C.gsize(len(d)), nil)
-	n := C.gst_buffer_fill(buf, C.gsize(0), (C.gconstpointer)(C.CBytes(d)), C.gsize(len(d)))
-
-	ret := FlowReturn(C.gst_app_src_push_buffer((*C.GstAppSrc)(a.GetPtr()), buf))
-	if ret != GST_FLOW_OK {
-		return 0, fmt.Errorf("appsrc push buffer failed: %v", ret)
-	}
-
-	return int(n), nil
+func (b *AppSrc) SetCaps(caps *Caps) {
+	C.gst_app_src_set_caps((*C.GstAppSrc)(b.g()), (*C.GstCaps)(caps))
 }
